5515dd37bc21bea9a2046ffb8f7b164323aecf58
Lavee Singh <archives987@gmail.com>
2017-09-17 17:28:39 +0530

    feat: add basic demo routes and MuiThemeProvider wrapper
    BrowserRouter has been added to handle url routing. Also some demo routes have been added just to test various components individually and independtly.
    Finally MuiThemeProvider has been added to wrap the entire application to support material ui themes.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  hashHistory,
+  Link,
+  Route,
+  Switch
+} from 'react-router-dom';
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
+import SignupForm from "./components/SignupForm";
+import MotiveCard from "./components/MotiveCard";
+import Register from "./components/Register";
 
 class App extends React.Component {
   render() {
     return (
-      <h1>I'll see you in a moment</h1>
+      <MuiThemeProvider>
+        <BrowserRouter history={hashHistory}>
+          <Switch>
+            <Route path="/demo/signup" component={SignupForm} />
+            <Route path="/demo/motivecard" component={MotiveCard} />
+            <Route path="/demo/register" component={Register} />
+          </Switch>
+        </BrowserRouter>
+      </MuiThemeProvider>
     );
   }
 }
