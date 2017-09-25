201061b7d54a39119af31301b270cdee4262a78d
Lavee Singh <archives987@gmail.com>
2017-09-25 18:28:12 +0530

    feat: add header navbar to register component, and add brand logo to navbar
    Added Navbar as header to Register component, and also added navbar logo.

--- src/components/Register.js
+++ src/components/Register.js
@@ -5,6 +5,7 @@ import MotiveCard from './MotiveCard'
 import SignupForm from './SignupForm'
 import PropTypes from 'prop-types'
 import {withStyles} from 'material-ui/styles'
+import Navbar from './Navbar'
 
 const styles = theme => ({
   motiveCard: {
@@ -21,26 +22,35 @@ class Register extends React.Component {
   constructor (props) {
     super(props)
     // maintain states below here
-    this.state = {}
+    this.state = {
+    }
   }
 
-  componentDidMount () {} // is invoked right after component is rendered
+  componentDidMount () {
+    //const logoUrl = require('../../public/logos/vstv-logo-v2.0-dark-withtitle.png')
+    this.setState({
+      appBarLeftMostChild: ( <img src='/logos/vstv-logo-v2.0-dark-withtitle.png' style={{ width: 100, height:40 }}/> )
+    })
+  }
 
   render () {
     const classes = this.props.classes
     return (
-      <Grid container >
-        <Grid item xs={12}>
-          <Grid container justify='center'>
-            <Grid item key={'0'} className={classes.motiveCard}>
-              <MotiveCard />
-            </Grid>
-            <Grid item key={'1'} className={classes.motiveCard}>
-              <SignupForm />
+      <div>
+        <Navbar leftMostChild={this.state.appBarLeftMostChild} />
+        <Grid container >
+          <Grid item xs={12}>
+            <Grid container justify='center'>
+              <Grid item key={'0'} className={classes.motiveCard}>
+                <MotiveCard />
+              </Grid>
+              <Grid item key={'1'} className={classes.motiveCard}>
+                <SignupForm />
+              </Grid>
             </Grid>
           </Grid>
         </Grid>
-      </Grid>
+      </div>
     )
   }
 }
