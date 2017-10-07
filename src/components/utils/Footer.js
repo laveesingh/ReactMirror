0d03bc0cbbbcd49f934a51c8ddda6c6bb5f8d585
lavee singh <laveechaudharytheone@gmail.com>
2017-10-07 23:05:55 +0530

    fix: resolve value and onchange flag error in footer tabs

--- src/components/utils/Footer.js
+++ src/components/utils/Footer.js
@@ -3,7 +3,7 @@ import Tabs, { Tab } from 'material-ui/Tabs'
 
 export function footer(props){
   return (
-    <Tabs>
+    <Tabs value={0} onChange={() => console.log("Footer tab clicked")}>
       <Tab label="About" />
       <Tab label="Careers" />
       <Tab label="Help" />
